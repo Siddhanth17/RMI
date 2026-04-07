293f7f4a657f70fff6c7cc0b54539411c0622b6d
Siddhanth <siddhanthdalal@gmail.com>
2026-04-07 14:08:42 +0530

    Chat Server

---
+++ src/rmi/ChatServer.java
@@ -0,0 +1,18 @@
+package rmi;
+
+import java.rmi.registry.LocateRegistry;
+
+public class ChatServer {
+    public static void main(String[] args) {
+        try {
+            Chatimpl obj = new Chatimpl();
+            Registry registry = LocateRegistry.createRegistry(2000);
+            registry.rebind("Chat Service", obj);
+            System.out.println("Chat Server is ready.");
+            
+        } catch (Exception e) {
+            System.err.println("Chat Server exception: " + e.toString());
+            e.printStackTrace();
+        }
+    }
+}
